b20a244c69716d31f52388e2c08dda45d39145e8
brecert <me@bree.dev>
2021-06-25 01:13:58 -0400

    allow selection of emoji styles

--- index.js
+++ index.js
@@ -3,28 +3,41 @@ const EMOJI = new RegExp(E.source, "gu");
 
 const input = document.getElementById("input");
 const output = document.getElementById("output");
+const styleForm = document.getElementById("styleForm");
 
 const config = {
-  emoji_url: "https://static.revolt.chat/emoji/mutant/",
+  emoji_url: "https://static.revolt.chat/emoji",
 };
 
-/** @param {string} text */
-const codepoints = (text) => [...text].map((c) => c.codePointAt(0));
+const state = {
+  emojiStyle: "mutant",
+};
 
-/** @param {string} text */
-const codepointString = (text) =>
-  codepoints(text)
+styleForm.oninput = (e) => {
+  state.emojiStyle = styleForm.elements.namedItem("emoji style").value;
+  update()
+};
+
+/** @param {string} emoji */
+const codepoints = (emoji) => [...emoji].map((c) => c.codePointAt(0));
+
+/** @param {string} emoji */
+const codepointString = (emoji) =>
+  codepoints(emoji)
     .map((c) => c.toString(16))
     .join("-");
 
 /** @param {string} text */
-const Emoji = (text) => {
+const Emoji = ({ emoji, style = state.emojiStyle } = {}) => {
   let el = document.createElement("span");
   el.classList.add("emoji");
-  el.innerText = text;
+  el.textContent = emoji;
 
-  let codepoints = codepointString(text);
-  el.style.backgroundImage = `url(${config.emoji_url}${codepoints}.svg)`;
+  let codepoints = codepointString(emoji);
+  el.style.setProperty(
+    "--emoji-url",
+    `url("${config.emoji_url}/${style}/${codepoints}.svg")`
+  );
 
   return el;
 };
@@ -37,7 +50,7 @@ function parseEmojis(text) {
   for (let match of matches) {
     let emoji = match[0];
     output.push(text.slice(lastPos, match.index));
-    output.push(Emoji(emoji));
+    output.push(Emoji({ emoji }));
     lastPos = match.index + emoji.length;
   }
   output.push(text.slice(lastPos));
@@ -52,3 +65,14 @@ function update() {
 
 input.oninput = update;
 update();
+
+class Breemoji extends HTMLElement {
+  constructor() {
+    super();
+    const style = this.dataset.style;
+    const emoji = Emoji({ style, emoji: this.dataset.emoji });
+    this.append(emoji);
+  }
+}
+
+customElements.define("bree-moji", Breemoji);
